96d7f0d5b88aae883348f3fe20604f7a617d12cd
Sachram Singh <singhsach1@ecs.vuw.ac.nz>
2025-04-19 09:42:28 +1200

    functionalised the alarm sound and http post requests, also added the network archetecture diagram and fixed some of the wording accordingly

--- ESP_32_Dependencies/Home_Sentinel.cpp
+++ ESP_32_Dependencies/Home_Sentinel.cpp
@@ -12,6 +12,11 @@ const int LED = 2;
 
 
 void setup() {
+  //set up pinModes for peripherals
+  pinMode(PIR_SENSOR_OUTPUT_PIN, INPUT); 
+  pinMode(ALARM,OUTPUT);
+  pinMode(LED, OUTPUT);
+
   Serial.begin(115200);
   delay(1000);
 
@@ -27,13 +32,45 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // Send test POST request
-  HTTPClient http;
-  http.begin(serverURL);  // Endpoint
-  http.addHeader("Content-Type", "application/json");  // Set content type
+  int server_Response_Code = post_Http("{\"test\":\"connection\"}");
 
-  String payload = "{\"test\":\"connection\"}";
-  int httpCode = http.POST(payload);  // Send POST
+  Serial.println("Response Code: " + server_Response_Code);
+  Serial.println("Waiting For Power On Warm Up");
+  delay(20000); /* Power On Warm Up Delay */
+
+  alarm_Armed();
+
+  Serial.println("Ready!");
+}
+
+
+void loop() {
+  int sensor_output;
+  sensor_output = digitalRead(PIR_SENSOR_OUTPUT_PIN);
+  if (sensor_output == LOW) {
+    if (warm_up == 1) {
+      Serial.print("Warming Up\n\n");
+      warm_up = 0;
+      digitalWrite(LED, LOW);
+      delay(20000);
+      alarm_Armed();
+    }
+    Serial.print("No object in sight\n\n");
+    delay(1000);
+  } else {
+    Serial.print("Object detected\n\n");
+    digitalWrite(LED, HIGH);
+    warm_up = 1;
+    post_Http("{\"ALARM\":\"connection\"}");
+  }
+}
+
+int post_Http(String payload){
+  HTTPClient http;  // <- Correct class
+  http.begin(serverURL);
+  http.addHeader("Content-Type", "application/json");
+
+  int httpCode = http.POST(payload);
 
   Serial.print("HTTP Response Code: ");
   Serial.println(httpCode);
@@ -45,12 +82,10 @@ void setup() {
   }
 
   http.end();
+  return httpCode;
+}
 
-  pinMode(PIR_SENSOR_OUTPUT_PIN, INPUT);
-  pinMode(ALARM,OUTPUT);
-  pinMode(LED, OUTPUT);
-  Serial.println("Waiting For Power On Warm Up");
-  delay(20000); /* Power On Warm Up Delay */
+void alarm_Armed(){
   digitalWrite(LED, LOW);
   noTone(ALARM);
 
@@ -67,54 +102,4 @@ void setup() {
   digitalWrite(LED, LOW);
   delay(150);
 }
-  Serial.println("Ready!");
-}
-
-
-void loop() {
-  int sensor_output;
-  sensor_output = digitalRead(PIR_SENSOR_OUTPUT_PIN);
-  if (sensor_output == LOW) {
-    if (warm_up == 1) {
-      Serial.print("Warming Up\n\n");
-      warm_up = 0;
-      digitalWrite(LED, LOW);
-      delay(20000);
-      for (int i = 0; i < 3; i++) {
-      tone(ALARM, 1000);
-      digitalWrite(LED, HIGH);
-      delay(150);
-      noTone(ALARM);
-      digitalWrite(LED, LOW);
-      delay(150);
-      digitalWrite(LED, HIGH);
-      delay(150);
-      noTone(ALARM);
-      digitalWrite(LED, LOW);
-      delay(150);
-}
-    }
-    Serial.print("No object in sight\n\n");
-    delay(1000);
-  } else {
-    Serial.print("Object detected\n\n");
-    digitalWrite(LED, HIGH);
-    warm_up = 1;
-    HTTPClient http;
-    http.begin(serverURL);  // Endpoint
-    http.addHeader("Content-Type", "application/json");  // Set content type
-
-    String payload = "{\"ALERT\":\"connection\"}";
-    int httpCode = http.POST(payload);  // Send POST
-
-    Serial.print("HTTP Response Code: ");
-    Serial.println(httpCode);
-
-    if (httpCode > 0) {
-      String response = http.getString();
-      Serial.print("Server Response: ");
-      Serial.println(response);
-    }
-    http.end();
-  }
 }
